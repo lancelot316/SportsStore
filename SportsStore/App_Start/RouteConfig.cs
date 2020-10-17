ee11412d921ed419754f628f8e72e64a1995391e
lenewman <lance.newman@elynxx.com>
2020-10-17 09:31:18 -0400

    Added Order Management

--- SportsStore/App_Start/RouteConfig.cs
+++ SportsStore/App_Start/RouteConfig.cs
@@ -13,6 +13,9 @@ namespace SportsStore.App_Start
 
             routes.MapPageRoute("cart", "cart", "~/Pages/CartView.aspx");
             routes.MapPageRoute("checkout", "checkout", "~/Pages/Checkout.aspx");
+
+            routes.MapPageRoute("admin_orders", "admin/orders", "~/Pages/Admin/Orders.aspx"); 
+            routes.MapPageRoute("admin_products", "admin/products", "~/Pages/Admin/Products.aspx");
         }
     }
 }
