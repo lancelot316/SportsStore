5065305b5b2648e32b855ef59585b25d5e418a11
lenewman <lance.newman@elynxx.com>
2021-05-09 10:22:54 -0400

    Added basic MVC product listing.

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using SportsStore.Models;
+using SportsStore.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private Repository repo;
+
+        public ProductController()
+        {
+            repo = new Repository();
+        }
+
+
+        // GET: Product
+        public ActionResult List()
+        {
+            IEnumerable<Product> products = repo.Products;
+
+            return View(products);
+        }
+    }
+}
