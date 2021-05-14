54c184f13702a814ebddfe9283b399165f5a6986
lenewman <lance.newman@elynxx.com>
2021-05-14 10:36:29 -0400

    removed webforms pages and controls from the project

--- SportsStore.WebUI/App_Start/RouteConfig.cs
+++ SportsStore.WebUI/App_Start/RouteConfig.cs
@@ -37,19 +37,7 @@ namespace SportsStore.WebUI.App_Start
                 new { page = @"\d+" }
             );
 
-            routes.MapRoute(null, "{controller}/{action}");
-
-            //routes.MapPageRoute(null, "list/{category}/{page}", "~/Pages/Listing.aspx");
-            //routes.MapPageRoute(null, "list/{page}", "~/Pages/Listing.aspx");
-            //routes.MapPageRoute(null, "", "~/Pages/Listing.aspx");
-            //routes.MapPageRoute(null, "list", "~/Pages/Listing.aspx");
-
-            //routes.MapPageRoute("cart", "cart", "~/Pages/CartView.aspx");
-            //routes.MapPageRoute("checkout", "checkout", "~/Pages/Checkout.aspx");
-
-            //routes.MapPageRoute("admin_orders", "admin/orders", "~/Pages/Admin/Orders.aspx");
-            //routes.MapPageRoute("admin_products", "admin/products", "~/Pages/Admin/Products.aspx");
-            
+            routes.MapRoute(null, "{controller}/{action}");            
         }
     }
 }
