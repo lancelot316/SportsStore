2edc60b8f750ed32233f8c7e8e4c4440226da90f
lenewman <lance.newman@elynxx.com>
2021-06-05 15:59:02 -0400

    fixed error in OrderLine sql script

--- Database/dbo.order_lines.sql
+++ Database/dbo.order_lines.sql
@@ -4,7 +4,7 @@
     [Quantity] INT NOT NULL, 
     [ProductId] INT NULL, 
     [OrderId] INT NULL, 
-    CONSTRAINT [FK_dbo.OrderLines_dbo_Products] FOREIGN KEY ([Product_ProductId]) REFERENCES [dbo].[Products]([ProductId]), 
-    CONSTRAINT [FK_dbo.OrderLines_dbo.Orders_Order_OrderId] FOREIGN KEY ([Order_OrderId]) REFERENCES [dbo].[Orders]([OrderId])
+    CONSTRAINT [FK_dbo.OrderLines_dbo_Products] FOREIGN KEY ([ProductId]) REFERENCES [dbo].[Products]([ProductId]), 
+    CONSTRAINT [FK_dbo.OrderLines_dbo.Orders_Order_OrderId] FOREIGN KEY ([OrderId]) REFERENCES [dbo].[Orders]([OrderId])
 
 )
